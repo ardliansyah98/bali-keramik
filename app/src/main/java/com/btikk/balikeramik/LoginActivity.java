009b39730ce3efb104542645c5ab18a3e9e8ac83
ardliansyah198 <ardliansyah198@gmail.com>
2023-03-19 05:58:17 +0800

    Very first commit

---
+++ app/src/main/java/com/btikk/balikeramik/LoginActivity.java
@@ -0,0 +1,39 @@
+package com.btikk.balikeramik;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.widget.Button;
+import android.widget.TextView;
+
+public class LoginActivity extends AppCompatActivity {
+
+    TextView tvToRegister;
+    Button btnLogin;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+
+        // Linking to xml objects
+        tvToRegister = (TextView) findViewById(R.id.TvToRegister);
+        btnLogin = (Button) findViewById(R.id.btnLogin);
+
+        // Go to Register Activity
+        tvToRegister.setOnClickListener(v -> {
+            startActivity(new Intent(this, RegistrasiActivity.class));
+        });
+
+        // Login Button
+        btnLogin.setOnClickListener(v -> {
+            login();
+        });
+    }
+
+    private void login() {
+        startActivity(new Intent(LoginActivity.this, DashboardActivity.class));
+        finish();
+    }
+}
