1de6578ad95bf0731d1cdf0c9dd1498781bb9409
ardliansyah198 <ardliansyah198@gmail.com>
2023-05-21 16:57:11 +0800

    Added functioning login functionalities etc

--- app/src/main/java/com/btikk/balikeramik/LoginActivity.java
+++ app/src/main/java/com/btikk/balikeramik/LoginActivity.java
@@ -4,22 +4,47 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.text.TextUtils;
+import android.util.Log;
 import android.widget.Button;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import com.android.volley.Request;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
+import com.btikk.balikeramik.configs.AppConfig;
 import com.btikk.balikeramik.configs.GetDate;
+import com.btikk.balikeramik.configs.MyVolleySingleton;
+import com.btikk.balikeramik.configs.SharedPrefManager;
+import com.btikk.balikeramik.models.User;
+import com.google.android.material.textfield.TextInputEditText;
+import com.google.android.material.textfield.TextInputLayout;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.HashMap;
+import java.util.Map;
 
 public class LoginActivity extends AppCompatActivity {
 
     TextView tvToRegister;
     Button btnLogin;
-    private GetDate getDate = new GetDate();
+    TextInputLayout TilEmail, TilPassword;
+    TextInputEditText etEmail, etPassword;
+    String email, password;
+    AppConfig appConfig = new AppConfig();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+        TilEmail = (TextInputLayout) findViewById(R.id.TilEmailLogin);
+        TilPassword = (TextInputLayout) findViewById(R.id.TilPasswordLogin);
+        etEmail = (TextInputEditText) findViewById(R.id.TxtEmailLogin);
+        etPassword = (TextInputEditText) findViewById(R.id.TxtPasswordLogin);
 
         // Linking to xml objects
         tvToRegister = (TextView) findViewById(R.id.TvToRegister);
@@ -37,7 +62,56 @@ public class LoginActivity extends AppCompatActivity {
     }
 
     private void login() {
-        startActivity(new Intent(LoginActivity.this, DashboardActivity.class));
-        //finish();
+        email = etEmail.getText().toString().trim();
+        password = etPassword.getText().toString().trim();
+        TilEmail.clearFocus();
+        TilPassword.clearFocus();
+
+        if (TextUtils.isEmpty(email)) {
+            TilEmail.setError("Email tidak boleh kosong");
+            TilEmail.requestFocus();
+        } else if (TextUtils.isEmpty(password)) {
+            TilPassword.setError("Password tidak boleh kosong");
+            TilPassword.requestFocus();
+        } else {
+            StringRequest stringRequest = new StringRequest(Request.Method.POST, appConfig.AuthUrl(), response -> {
+                try {
+                    JSONObject jsonObject = new JSONObject(response);
+                    if(jsonObject.optString("error").equals("false")){
+                        // success
+                        Intent intent = new Intent(this, DashboardActivity.class);
+                        JSONObject userJson = jsonObject.getJSONObject("akun");
+                        User user = new User(userJson.getInt("id"),
+                                userJson.getInt("id_perajin"),
+                                userJson.getInt("is_active"),
+                                userJson.getString("nama"),
+                                userJson.getString("email"),
+                                userJson.getString("no_telp"),
+                                userJson.getString("foto_profil"),
+                                userJson.getString("date_created"));
+                        SharedPrefManager.getInstance(this).userLogin(user);
+                        finish();
+                        startActivity(intent);
+                        Log.d("User Account", userJson.toString());
+                    } else {
+                        Toast.makeText(this, "Error: " + jsonObject.getString("message"), Toast.LENGTH_SHORT).show();
+                    }
+                } catch (JSONException e) {
+                    throw new RuntimeException(e);
+                }
+            }, error -> error.printStackTrace()) {
+                @Override
+                protected Map<String, String> getParams() {
+                    Map<String, String> params = new HashMap<>();
+                    params.put("login", "1");
+                    params.put("email", email);
+                    params.put("password", password);
+                    return params;
+                }
+            };
+            //startActivity(new Intent(LoginActivity.this, DashboardActivity.class));
+            //finish();
+            MyVolleySingleton.getInstance(this).addToRequestQueue(stringRequest);
+        }
     }
 }
--- app/src/main/java/com/btikk/balikeramik/fragments/AccountFragment.java
+++ app/src/main/java/com/btikk/balikeramik/fragments/AccountFragment.java
@@ -2,16 +2,30 @@ package com.btikk.balikeramik.fragments;
 
 import android.os.Bundle;
 
+import androidx.core.widget.NestedScrollView;
 import androidx.fragment.app.Fragment;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.RelativeLayout;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import com.btikk.balikeramik.R;
+import com.btikk.balikeramik.configs.SharedPrefManager;
+import com.btikk.balikeramik.models.User;
 
 public class AccountFragment extends Fragment {
+    private NestedScrollView svAccount;
+    private RelativeLayout layoutNotLoggedIn;
+    private TextView tvDate;
+    private TextView tvEmail1;
+    private TextView tvNama;
+    private TextView tvNamaLengkap;
+    private TextView tvPerajin;
+    private TextView tveEmail;
+
 
     public AccountFragment() {
         // Required empty public constructor
@@ -22,6 +36,29 @@ public class AccountFragment extends Fragment {
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         View view = inflater.inflate(R.layout.fragment_account, container, false);
+        svAccount = (NestedScrollView) view.findViewById(R.id.sv_account);
+        layoutNotLoggedIn = (RelativeLayout) view.findViewById(R.id.not_logged_in);
+        this.tvNama = (TextView) view.findViewById(R.id.txt_nama);
+        this.tveEmail = (TextView) view.findViewById(R.id.txt_email);
+        this.tvDate = (TextView) view.findViewById(R.id.txt_tanggal_daftar);
+        this.tvNamaLengkap = (TextView) view.findViewById(R.id.txt_nama_lengkap);
+        this.tvEmail1 = (TextView) view.findViewById(R.id.txt_email_1);
+
+        // if user logged in
+        if(SharedPrefManager.getInstance(getActivity().getApplicationContext()).isLoggedIn()){
+            layoutNotLoggedIn.setVisibility(View.GONE);
+            final User user = SharedPrefManager.getInstance(getActivity().getApplicationContext()).getUser();
+            this.tvNama.setText(user.getNama());
+            this.tveEmail.setText(user.getEmail());
+            this.tvDate.setText(user.getDateCreated());
+            this.tvNamaLengkap.setText(user.getNama());
+            this.tvEmail1.setText(user.getEmail());
+
+        } else if(!SharedPrefManager.getInstance(getActivity().getApplicationContext()).isLoggedIn()){
+            layoutNotLoggedIn.setVisibility(View.VISIBLE);
+            svAccount.setVisibility(View.GONE);
+        }
+
         return view;
     }
 }
